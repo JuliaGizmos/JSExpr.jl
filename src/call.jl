2d43995144e1bacdf8d1de1e21eb4fef948eff26
Travis DePrato <travisdeprato@gmail.com>
2019-10-25 19:16:36 -0400

    Refactor some details around crawling function calls.

--- src/call.jl
+++ src/call.jl
@@ -1,19 +1,34 @@
-# Generically, the method may be any expression
-# e.g. In ``@js console.log("foo")`, the method has it's own AST since it's
-# using the dot-operator (`.`).
-crawl(h::Val{:call}, f, args...) = :(
-    JSAST(
+# See documentation for `crawl_call` for documentation on what we're doing here.
+function crawl(::Val{:call}, f, args...)
+    return :(JSAST(
         :call,
         $(crawl(f)),
         $(crawl.(args)...),
-    )
-)
+    ))
+end
+function crawl(::Val{:call}, f::Symbol, args...)
+    return crawl_call(f, args...)
+end
 
-# But for simple cases, we can allow for dispatching based on the method name.
-crawl(h::Val{:call}, m::Symbol, args...) = crawl(h, Val(m), args...)
+"""
+Crawl a "simple" function call.
+
+In general, the function may be any expression (e.g., in
+`@js console.log("foo")`, the function `console.log` is not a symbol and has its
+own JSAST since it's using the dot-operator). However, for simple cases, when
+the function is just a symbol, we can dispatch to the `crawl_call` function to
+perform further transformation if necessary.
+
+This is useful for transforming function calls which are represented by Julia as
+function calls (e.g. we transform `+(1, 2)` into `1 + 2`), as well as for
+translating some Julia-isms.
+"""
+function crawl_call(f::Symbol, args...)
+    return crawl_call(Val(f), args...)
+end
 
 # Default for generic methods
-crawl(::Val{:call}, ::Val{M}, args...) where {M} = :(
+crawl_call(::Val{M}, args...) where {M} = :(
     JSAST(
         :call,
         $(crawl(M)),
@@ -21,6 +36,6 @@ crawl(::Val{:call}, ::Val{M}, args...) where {M} = :(
     )
 )
 
-function deparse(::Val{:call}, m, args...)
-    jsstring(deparse(m), "(", join(deparse.(args), ", "), ")")
+function deparse(::Val{:call}, f, args...)
+    jsstring(deparse(f), "(", join(deparse.(args), ", "), ")")
 end
--- src/infix.jl
+++ src/infix.jl
@@ -1,4 +1,5 @@
-# "Standard" infix operators
+# These infix operators have their own distinct head symbols in Julia's AST and
+# so we can crawl them as part of the "standard" crawl function.
 for infix in (:(=), :(+=), :(-=), :(/=), :(*=))
     # The inner QuoteNode is required so that @eval passes the operator
     # through as a symbol (and not directly interpolating it), and the outer
@@ -7,40 +8,51 @@ for infix in (:(=), :(+=), :(-=), :(/=), :(*=))
     # interpolated.
     sym = QuoteNode(infix)
     @eval begin
-        crawl(h::Val{$sym}, lhs, rhs) = Expr(
-            :call,
-            :JSAST,
-            QuoteNode($sym),
-            crawl(lhs),
-            crawl(rhs),
-        )
-        deparse(h::Val{$sym}, lhs, rhs) = jsstring(
-            deparse(lhs), " ", string($sym), " ", deparse(rhs),
-        )
+        function crawl(h::Val{$sym}, lhs, rhs)
+            # NOTE: We use the `Expr` constructor hear instead of the :(...)
+            # syntax to make interpolation easier here.
+            return Expr(
+                :call,
+                :JSAST,
+                QuoteNode($sym),
+                crawl(lhs),
+                crawl(rhs),
+            )
+        end
+        function deparse(h::Val{$sym}, lhs, rhs)
+            return jsstring(
+                deparse(lhs), " ", string($sym), " ", deparse(rhs),
+            )
+        end
     end
 end
 
-# Take care of infix operators that are translated to calls in the Julia AST
-# (We need to translate them back into infix operators for the JSAST).
+# These infix operators are represented as function calls in Julia's AST and so
+# we need to crawl them as part of the `crawl_call` process and deparse them
+# into the appropriate infix for JavaScript.
 for infix in (:+, :-, :*, :/, :in, :of)
     sym = QuoteNode(infix)
     @eval begin
-        crawl(::Val{:call}, ::Val{$sym}, lhs, rhs) = Expr(
-            :call,
-            :JSAST,
-            QuoteNode($sym),
-            crawl(lhs),
-            crawl(rhs),
-        )
-        deparse(h::Val{$sym}, lhs, rhs) = jsstring(
-            deparse(lhs), " ", string($sym), " ", deparse(rhs),
-        )
+        function crawl_call(::Val{$sym}, lhs, rhs)
+            return Expr(
+                :call,
+                :JSAST,
+                QuoteNode($sym),
+                crawl(lhs),
+                crawl(rhs),
+            )
+        end
+        function deparse(h::Val{$sym}, lhs, rhs)
+            return jsstring(
+                deparse(lhs), " ", string($sym), " ", deparse(rhs),
+            )
+        end
     end
 end
 
 function crawl(h::Val{:(.)}, lhs, rhs)
-    :(JSAST(:(.), $(crawl(lhs)), $(crawl(rhs))))
+    return :(JSAST(:(.), $(crawl(lhs)), $(crawl(rhs))))
 end
 function deparse(h::Val{:(.)}, lhs, rhs)
-    js"$(deparse(lhs)).$(deparse(rhs))"
+    return js"$(deparse(lhs)).$(deparse(rhs))"
 end
