0ba82b3f0cfbddf6f200fd3455a9d40af309fb23
Travis DePrato <travisdeprato@gmail.com>
2019-10-25 23:03:06 -0400

    Add more infix operators.

--- src/infix.jl
+++ src/infix.jl
@@ -1,6 +1,14 @@
+# For the definitive list of infix operators, examine the Julia parser code:
+# https://github.com/JuliaLang/julia/blob/master/src/julia-parser.scm#L9
+# Most of the infixes are omitted since JavaScript doesn't allow arbitrary
+# unicode identifiers and/or the respective infixes don't exist in JavaScript.
+
 # These infix operators have their own distinct head symbols in Julia's AST and
 # so we can crawl them as part of the "standard" crawl function.
-for infix in (:(=), :(+=), :(-=), :(/=), :(*=))
+for infix in (
+        :(=), :(+=), :(-=), :(/=), :(*=),
+        :(&&), :(||),
+)
     # The inner QuoteNode is required so that @eval passes the operator
     # through as a symbol (and not directly interpolating it), and the outer
     # QuoteNode is required so that when crawl returns the Expr, the
@@ -30,7 +38,11 @@ end
 # These infix operators are represented as function calls in Julia's AST and so
 # we need to crawl them as part of the `crawl_call` process and deparse them
 # into the appropriate infix for JavaScript.
-for infix in (:+, :-, :*, :/, :in, :of, :(==), :(!=), :(===), :(!==))
+for infix in (
+        :+, :-, :*, :/,
+        :in, :of, :(==), :(!=), :(===), :(!==),
+        :(>), :(<), :(>=), :(<=),
+)
     sym = QuoteNode(infix)
     @eval begin
         function crawl_call(::Val{$sym}, lhs, rhs)
