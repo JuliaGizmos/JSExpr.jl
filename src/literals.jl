6a6bbbbd002273df633572710cd93f5f838a96d7
Travis DePrato <travisdeprato@gmail.com>
2019-10-27 14:51:32 -0400

    Make crawl_symbol consistent with other sub-crawl methods.

--- src/literals.jl
+++ src/literals.jl
@@ -4,11 +4,11 @@ crawl(b::Bool) = crawl(Symbol(b))
 
 # Symbols
 # We sub-dispatch on symbols to allow for special handling of certain keywords.
-crawl(sym::Symbol) = crawl(Val(:symbol), Val(sym))
-function crawl(::Val{:symbol}, sym::Val{S}) where {S}
+crawl(sym::Symbol) = crawl_symbol(Val(sym))
+function crawl_symbol(sym::Val{S}) where {S}
     return :(JSTerminal($(QuoteNode(S))))
 end
 deparse(sym::JSTerminal)::JSString = sym.s
 
 # Convert the nothing symbol into null.
-crawl(::Val{:symbol}, ::Val{:nothing}) = :(JSTerminal(nothing))
+crawl_symbol(::Val{:nothing}) = :(JSTerminal(nothing))
